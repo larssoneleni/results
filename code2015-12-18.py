35938d8ad9d37af957f06613e1938ad3731b28da
Viktor Sas <vsas@kth.se>
2015-12-18 13:03:45 +0100

    codes

--- code2015-12-18.py
+++ code2015-12-18.py
@@ -2,11 +2,12 @@
 #coding: utf8
 #####
 # We can run muscle on every file except the tree-files to get a better order??? 
-# (använda seaview för att få en bild av hur sekvenserna (eller linjeringen/alignments) ser ut där vi även kan se hur kolumnerna. 
-# Så ett tänkbar start till programmet är att input blir filerna och köra muscle och få ut output-filer.
-# Dess output-filer ska sedan granskas utifrån 3 kriterier!
-# 
-#
+#(use seaview to get a picture of how the sequences (or the alignments)
+# looks like where we can se how the columns looks.
+# A starting point for the program is where our input is the files 
+# from our data directory (except the tree-files, challange!) and 
+#run muscle to get outputfiles.
+# Next things is to check each column (from our outputfile) with our 3 criterias!
 ####
 
 
