9cbc39261b6762e695566996f6077c5b29bb3f46
Viktor Sas <vsas@kth.se>
2015-12-18 12:57:02 +0100

    code

---
+++ code2015-12-18.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#coding: utf8
+#####
+# We can run muscle on every file except the tree-files to get a better order??? 
+# (använda seaview för att få en bild av hur sekvenserna (eller linjeringen/alignments) ser ut där vi även kan se hur kolumnerna. 
+# Så ett tänkbar start till programmet är att input blir filerna och köra muscle och få ut output-filer.
+# Dess output-filer ska sedan granskas utifrån 3 kriterier!
+# 
+#
+####
+
+
+
+
